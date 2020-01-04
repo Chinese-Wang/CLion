6a837ecd2c9b2e61c5854a658d1162d2c6877323
Chinese-Wang <42296962+Chinese-Wang@users.noreply.github.com>
2020-01-04 16:30:10 +0800

    2019/10/20

--- LongestSubSeq.cpp
+++ LongestSubSeq.cpp
@@ -2,8 +2,6 @@
 // Created by 王京珠 on 2019/12/27.
 //
 
-//寻找最长回文子序列
-
 #include <iostream>
 #include <cstring>
 using namespace std;
@@ -11,91 +9,89 @@ using namespace std;
 #define MAXSIZE 100  //输入字符串最大长度
 
 /**
- * 蛮力法求解最长回文子序列
- * str 目标字符串
- * strLen 目标字符串长度
- * 返回值 找到的最长回文子序列
- * @return  char*
+ * 蛮力法寻找最长回文子序列
+ * @param str 要搜索的数组
+ * @param strLen 数组长度
+ * @param ans 结果数组
  */
-char* BruteForceLongest(const char *str,int strLen) {
-    char *ans = new char[strLen];  //结果数组
-    int ansLen = 0;   //数组长度
-
-    if(strLen < 2) {
+char* BruteForceLongest(const char *str,int strLen,char ans[]) {
+    if (strLen < 2) {  //要搜索的字符串长度小于2
         ans[0] = str[0];
         ans[1] = '\0';
         return ans;
     }
 
-    char *temp = new char[strLen];
+    int ansLen = 0;
+    char temp[MAXSIZE];   //暂存结果数组
 
-    for (int i = 0; i < strLen; ++i) {  //以首字母为回文串开始字符
+    for (int i = 0; i < strLen; ++i){  //以首字母为回文串开始字符
         for (int j = strLen - 1; j > i; --j) {   //找到与首字母相对应的字符
-            int tempLen = 0;   //找到的回文子序列长度
-            int k = 0;         //找到的回文字母对数
-            temp[k++] = str[i];  //假设第一个元素是回文的组成部分，将其放入temp数组中，k++
-            tempLen++;     //回文长度加1
-            if (str[i] == str[j]){  //该字符确实是回文的组成部分
-                tempLen++;        //回文字符串长度加1
-                int index1 = i + 1;    //index1指向回文首部的后一个
-                int index2 = j - k;    //index2指向查找回文串的末尾
-                while (index1 <= index2){     //查找首尾对应中的回文串
-                    if (index1 == index2){   //如果要查找回文串只剩一个字符
-                        temp[k++] = str[index1];  //将其放入已识别数组中
-                        tempLen += 1;
-                        break;
+            if (str[i] == str[j]) {  //该字符确实是回文的组成部分
+                int tempLen = 2;        //回文字符串长度为2，最外层的回文字符
+                int num = 0;    //已匹配出的回文字符对数
+                temp[num++] = str[i];  //将已匹配的回文字符放入数组temp中
+                int index1;    //index1指向回文开始处
+                int index2;    //index2指向回文串的末尾
+                for (int l = 0; l < j - i; ++l) {  //循环j-i次，将每一个字符可能组成的回文都寻找一遍
+                    int tempLen1 = tempLen;   //临时变量，暂存tempLen的值
+                    int tempIndex1 = i + l + 1;  //临时变量，表示每次开始寻找回文序列时的下标
+                    int tempIndex2 = j-1;  //保存上次回文序列已匹配的结束下标
+                    int k = num;  //临时变量，暂存已匹配的回文字符对数
+                    index1 = tempIndex1;  //index1向后移动
+                    index2 = tempIndex2; //index2向前移动
+                    while (index1 <= index2) {     //查找首尾对应中的回文串
+                        if (index1 == index2) {   //如果要查找回文串只剩一个字符
+                            temp[k++] = str[index1];  //将其放入已识别数组中
+                            tempLen1 += 1; //数组长度加一
+                            break;
+                        }
+                        while (str[index1] != str[index2] && index2 > index1) index2--;  //从尾部开始找与开始出相同的元素
+                        if (str[index1] == str[index2] && index1 != index2) {   //找到值相同但下标不同的元素
+                            temp[k++] = str[index1];    //将元素放到数组中
+                            tempLen1 += 2;    //长度加二
+                            index2--;  //尾部向前移动
+                            tempIndex2 = index2;
+                        } else
+                            index2 = tempIndex2;  //没有找到与首字符相同的元素，尾部移动到已识别尾部位置
+                        index1++;  //首部向前移动
                     }
-                    while (str[index1] != str[index2] && index2 > index1) index2--;  //从尾部开始找与开始出相同的元素
-                    if (str[index1] == str[index2] && index1 != index2) {   //找到值相同但下标不同的元素
-                        temp[k++] = str[index1];    //将元素放到数组中
-                        tempLen += 2;    //长度加二
-                        index2--;  //尾部向前移动
+                    if (tempLen1 > ansLen) {   //如果新找到的回文序列长度大于之前的长度
+                        ansLen = tempLen1;
+                        for (int copyIndex = 0; copyIndex < k; ++copyIndex) { //将元素复制到结果数组
+                            ans[copyIndex] = temp[copyIndex];
+                            ans[tempLen1 - copyIndex - 1] = temp[copyIndex];
+                        }
+                        ans[ansLen] = '\0';
                     }
-                    else
-                        index2 = j - k;  //没有找到与首字符相同的元素，尾部移动到已识别尾部位置
-                    index1++;  //首部向前移动
                 }
-            }
-            if (tempLen > ansLen) {   //如果新找到的回文序列长度大于之前的长度
-                ansLen = tempLen;
-                for (int l = 0; l < k; ++l) { //将元素复制到结果数组
-                    ans[l] = temp[l];
-                }
-                for (int l = 0; l < tempLen - k; ++l) {
-                    ans[l + k] = temp[tempLen - k - l - 1];
-                }
-                ans[ansLen] = '\0';
             }
         }
     }
-    delete [] temp;  //释放temp数组空间
-
+    if(ansLen == 0){
+        ans[0] = str[0];
+        ans[1] = '\0';
+    }
     return ans;
 }
 
 /**
  * 动态规划法求解最长回文子序列
- * str 目标字符串
- * strLen 目标字符除串长度
- * dp[i][j]  从i到j的最长回文子序列
- * status[i][j]  回溯矩阵，指明回溯的方向
- *              1 向对角线回溯    i+1,j-1
- *              2 向前回溯   i,j-1
- *              3 向下回溯   i+1,j
- * @return char*
+ * @param str 要搜索的字符串
+ * @param strLen 字符串长度
+ * @param ans 结果数组
  */
-char* DynProLongest(char *str,int strLen){
-    char *ans = new char[strLen];  //结果数组
+void DynProLongest(const char *str,int strLen,char ans[]){
     int ansLen = 0;   //数组长度
 
     if(strLen < 2) {   //如果原字符长度小于2
         ans[0] = str[0];
         ans[1] = '\0';
-        return ans;
+        return;
     }
-
-    int dp[MAXSIZE][MAXSIZE];   //长度数组，元素初值全为0
-    int status[MAXSIZE][MAXSIZE];   //状态数组，元素初值全为0
+    //长度数组，元素初值全为0，dp[i][j]表示从i到j的字符串中的最长回文子序列的长度
+    int dp[MAXSIZE][MAXSIZE];
+    //状态数组，指明元素回溯的方向，1 向对角线方向回溯（i+1,j-1）,2 向前回溯（i，j-1），3 向下回溯（i+1，j）
+    int status[MAXSIZE][MAXSIZE];
 
     for (int i = strLen-1; i >=0 ; --i){  //从后往前遍历数组
         dp[i][i] = 1;    //单个元素看作是回文
@@ -131,11 +127,10 @@ char* DynProLongest(char *str,int strLen){
         ans[ansLen++] = ans[index - i];
     }
     ans[ansLen] = '\0';
-    return ans;
 }
 
 /**
- *
+ * 打开文件，返回文件指针
  * @return FILE*
  */
 FILE* fileRead()
@@ -157,34 +152,23 @@ FILE* fileRead()
  * @return char
  */
 char readLine(FILE* fp,char* str){
-    char ch = fgetc(fp);  //先读取一个元素
+    char ch = (char)fgetc(fp);  //先读取一个元素,fgetc()返回值为int，返回读入字符的ASCII码
     int i = 0;
     while(ch != EOF && ch != '\n'){   //文件到末尾或到了换行符
         str[i] = ch;
-        ch = fgetc(fp);
+        ch = (char)fgetc(fp);
         i++;
     }
     str[i] = '\0';
     return ch;
 }
-/*
-void randStr(){
-    char str[11];
-    int num;
-    for (int i = 0; i < 20; ++i) {
-        for (int j = 0; j < 10; ++j) {
-            num =  rand() % 7;
-            str[j] = (char)(num + 97);
-        }
-        str[10]  = '\0';
-        cout<<str<<endl;
-    }
-}*/
 
 int main()
 {
-    char str[MAXSIZE];
-    char ch = ' ';
+    char str[MAXSIZE];  //保存要搜索的字符串
+    char bfl[MAXSIZE];  //蛮力法搜索的结果
+    char dpl[MAXSIZE];  //动态规划法搜索的结果
+    char ch = ' ';  //退出读取文件的标志
 
     FILE *fp = fileRead();   //打开文件
     if(fp == nullptr){      //打开文件失败
@@ -195,13 +179,11 @@ int main()
         while(ch != EOF){   //文件没有读到末尾
             ch = readLine(fp,str);   //每次读取一行，将数据读到str字符数组中
             int strLen =strlen(str);   //获取字符串长度
-            char* bfl = BruteForceLongest(str,strLen);  //蛮力法求解出的最长回文子序列
-            char* dpl= DynProLongest(str,strLen);
+            BruteForceLongest(str,strLen,bfl);  //蛮力法求解出的最长回文子序列
+            DynProLongest(str,strLen,dpl);
             printf("%-15s",str);    //输出目标字符串
             printf("%-15s",bfl);   //输出找到的最长回文串
             printf("%-15s\n",dpl);   //输出找到的最长回文串
-            delete [] bfl;
-            delete [] dpl;
         }
         fclose(fp);
     }
